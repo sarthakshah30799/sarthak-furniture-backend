f5e7b28300187c79862b43a80cde385e31071b3e
sarthakshah30799 <sarthakshah30799@gmail.com>
2022-03-11 23:25:46 +0530

    port changes push

--- src/postgresql/home.ts
+++ src/postgresql/home.ts
@@ -1,9 +1,10 @@
 import * as pg from "pg";
 
+const port: any = process.env.PORT || 8000;
 const postgresqlConnection = () => {
   const client = new pg.Client({
     host: process.env.PG_HOST || "localhost",
-    port: 8000,
+    port,
     user: process.env.PG_USER || "postgres",
     password: process.env.PG_PASSWORD || "root",
     database: process.env.PG_DATABASE || "sarthak_furniture",
