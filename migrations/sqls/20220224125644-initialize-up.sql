2d1c64af6debcfc5b8ec7e35d6c34183da72dbe8
sarthakshah30799 <sarthakshah30799@gmail.com>
2022-04-16 23:49:34 +0530

    update-typo-&-auto-id-changes

--- migrations/sqls/20220224125644-initialize-up.sql
+++ migrations/sqls/20220224125644-initialize-up.sql
@@ -1,9 +1,8 @@
 /* Replace with your SQL commands */
 CREATE TABLE public."chairs"
 (
-    id uuid PRIMARY KEY,
-    name character varying COLLATE pg_catalog
-    ."default",
+    id uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name character varying,
     type character varying,
     url character varying,
     price money,
@@ -15,7 +14,7 @@ CREATE TABLE public."chairs"
     seatColor character varying,
     seatMaterial character varying,
     headRest boolean,
-    heightAdujstable character varying,
+    heightAdjustable character varying,
     pushback character varying,
     stand CHARACTER VARYING,
     wheel CHARACTER VARYING
---
+++ migrations/sqls/20220326105511-initialize-up.sql
@@ -0,0 +1,4 @@
+/* Replace with your SQL commands */
+/* Replace with your SQL commands */
+
+ALTER TABLE chairs RENAME heightAdujstable TO heightAdjustable;
